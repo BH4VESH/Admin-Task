79e670a1ea6ec5e010f73a197e73256221d08c15
mr.narola <110708712+BH4VESH@users.noreply.github.com>
2024-06-25 09:38:15 +0530

    Delete server directory

--- server/routes/countryRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const countryController = require('../controllers/countryController');
-
-router.get('/get', countryController.getAllCountries);
-router.post('/add', countryController.createCountry);
-
-module.exports = router;
